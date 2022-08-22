29c25701f135233cf0ca5785b527a29af9c40cb7
abhaySingh <38357472+ARS1337@users.noreply.github.com>
2022-08-22 12:32:41 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,35 @@
+var buffer = require('buffer');
+var udp = require('dgram');
+// creating a client socket
+var client = udp.createSocket('udp4');
+
+
+
+client.on('message',function(msg,info){
+  console.log('Data received from server : ' + msg.toString());
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+});
+
+console.log('before sending',Date.now())
+
+//sending msg
+client.send(Buffer.from(Date.now().toString()),2222,'localhost',function(error){
+    console.log('buffer sent',Date.now())
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
+
+var data1 = Buffer.from('hello');
+var data2 = Buffer.from('world');
+
+//sending multiple msg
+client.send([Buffer.from(`${Date.now()}`)],2222,'localhost',function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
